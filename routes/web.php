17dbd2fee8250742853d90aa68f974b670c6a3ea
salma asma nadia <salmasmanadia@gmail.com>
2026-06-17 13:17:54 +0700

    feat: add authentication and RBAC

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::view('/admin/dashboard', 'admin.dashboard')
+        ->name('admin.dashboard');
+});
+
+Route::middleware(['auth', 'role:mitra'])->group(function () {
+    Route::view('/mitra/dashboard', 'mitra.dashboard')
+        ->name('mitra.dashboard');
+});
+
+Route::middleware(['auth', 'role:peserta'])->group(function () {
+    Route::view('/peserta/dashboard', 'peserta.dashboard')
+        ->name('peserta.dashboard');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
